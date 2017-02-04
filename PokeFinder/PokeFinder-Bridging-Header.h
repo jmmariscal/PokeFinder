622ce171e366beaa767829c987af44dd03a2acc4
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-02-03 20:56:17 -0800

    Finished adding map and droping random pokemon

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Juan M Mariscal on 2/2/17.
+//  Copyright © 2017 Juan M Mariscal. All rights reserved.
+//
+#import "GeoFire.h"
